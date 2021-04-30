a4fd8eef2fc5254b8ec0915ce473ff05457d8861
rajat-ds <rajatkumarbind@gmail.com>
2021-05-01 00:44:08 +0530

    negation approach of missing and repeating number

--- arrays/repeating_and_missing_number.js
+++ arrays/repeating_and_missing_number.js
@@ -10,3 +10,33 @@
  * time -> O(n)
  * s->  0(n)
  */
+
+/**
+ * Method 3 (Use elements as Index and mark the visited places)
+Approach: 
+Traverse the array. While traversing, use the absolute value of 
+every element as an index and make the value at this index as negative to mark it visited.
+ If something is already marked negative then this is the repeating element.
+  To find missing, traverse the array again and look for a positive value.
+ */
+
+  /**
+   * @param {Array} arr 
+   * @author Rajat Kumar
+   */
+  const printMissingRepeating = (arr) => {
+    arr.forEach((ele, index) => {
+      let abs = Math.abs(arr[index]);
+      if (arr[abs - 1] > 0) {
+        arr[abs - 1] = -arr[abs - 1];
+      } else {
+        console.log("Repeating number is ->", abs);
+      }
+    });
+
+    arr.forEach((val, index) => {
+      if (val > 0) console.log("Missing number--->", index + 1);
+    });
+  };
+  const arr = [7, 3, 4, 5, 5, 6, 2];
+  printMissingRepeating(arr);
