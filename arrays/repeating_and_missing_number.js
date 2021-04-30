b88353395e4d73bf2e4ff42da94107ac53970127
rajat-ds <rajatkumarbind@gmail.com>
2021-04-30 23:43:35 +0530

    naive approach of missing and repeating number

---
+++ arrays/repeating_and_missing_number.js
@@ -0,0 +1,12 @@
+// Given an unsorted array of size n.
+//  Array elements are in the range from 1 to n. 
+//  One number from set {1, 2, …n} is missing and
+//   one number occurs twice in the array. Find these two numbers.
+
+/**
+ * Naive soltuon 
+ * Create an empty array of same length having zero
+ * iterrate through firdt array and increase the frequency count in secon
+ * time -> O(n)
+ * s->  0(n)
+ */
