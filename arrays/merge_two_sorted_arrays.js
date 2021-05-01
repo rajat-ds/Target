970ec32af10b9cc6324f5f17c4e8ff1965f74de0
rajat-ds <rajatkumarbind@gmail.com>
2021-05-01 15:48:14 +0530

    merge two sorted Arrays using insertion intuition

--- arrays/merge_two_sorted_arrays.js
+++ arrays/merge_two_sorted_arrays.js
@@ -3,6 +3,8 @@
  */
 const arr1 = [2, 5, 7, 13, 17];
 const arr2 = [1, 6, 15];
+console.log("arr1->",arr1);
+console.log("arr2->",arr2)
 /**
  * Naive approach
  * use an extra array
@@ -27,4 +29,30 @@ const mergeSortedArraysNaive = (arr1, arr2) => {
 
     return { arr1, arr2 };
 };
-console.log(mergeSortedArraysNaive(arr1, arr2));
+//console.log(mergeSortedArraysNaive(arr1, arr2));
+
+/**
+ * Insertion sort intuition
+ * @param {Array} arr1 
+ * @param {Array} arr2 
+ * @returns obj
+ * @author Rajat Kumar
+ */
+const mergeSortedArrayInsertion =(arr1,arr2)=>{
+      arr1.forEach((element,index)=>{
+          let temp=0;
+          if(element>arr2[0]){
+               temp=element;
+               arr1[index]=arr2[0]
+               arr2[0]=temp;
+               arr2.sort((a, b) => a - b);
+          }
+
+      })
+
+      return {arr1,arr2}
+    
+
+}
+
+console.log(mergeSortedArrayInsertion(arr1,arr2))
