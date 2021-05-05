28336ac5fa293417b5f506df50f11a862d44b35b
rajat-ds <rajatkumarbind@gmail.com>
2021-05-06 02:26:24 +0530

    set matrix zeros

---
+++ day2-Arrays/setZerosMatrix.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+const matrix =
+[[-4,-2147483648,6,-7,0],[-8,6,-8,-6,0],[2147483647,2,-9,-6,-10]]
+ var setZeroes = function(matrix) {
+    let Col = false;
+    for(let i =0 ; i<matrix.length ;i++){
+        if(matrix[i][0]==0)
+            Col = true;
+        for(let j =1 ;j<matrix[0].length ; j++){
+            if(matrix[i][j]===0)
+                matrix[i][0]=matrix[0][j]=0;
+        }
+    }
+     
+    for( let i = matrix.length-1 ;i>=0 ; i--){
+        for(let j = matrix[0].length -1 ;j>=0 ;j--){
+             if(!matrix[i][0] || !matrix[0][j])
+                 matrix[i][j]=0;
+            
+            
+        }
+        if(Col)
+            matrix[i][0]=0;
+        
+        
+    } 
+     
+     
+     
+ };
+setZeroes(matrix)
+ console.log(matrix)
