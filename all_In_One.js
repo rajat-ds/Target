2754f137f5238fb66e362032b8fc7a5a6db64d22
rajat-ds <rajatkumarbind@gmail.com>
2021-05-10 01:57:23 +0530

    next Permutation

--- all_In_One.js
+++ all_In_One.js
@@ -184,4 +184,29 @@ const printPascalTriangle2 = (n) => {
   return ans;
 };
 
-console.log(printPascalTriangle2(3));
+const nextPermutation = (nums) => {
+  function reverse(start) {
+    let end = nums.length - 1;
+    while (start < end) {
+      [nums[start], nums[end]] = [nums[end], nums[start]];
+      start++;
+      end--;
+    }
+  }
+
+  let i = nums.length - 2;
+  while (i >= 0 && nums[i + 1] <= nums[i]) {
+    i--;
+  }
+  if (i < 0) {
+    reverse(0);
+    return;
+  }
+  let j = nums.length - 1;
+  while (j >= 0 && nums[j] <= nums[i]) {
+    j--;
+  }
+  [nums[i], nums[j]] = [nums[j], nums[i]];
+  reverse(i + 1);
+  return nums;
+};
---
+++ day2-Arrays/nextPermutation.js
@@ -0,0 +1,31 @@
+const nums= [1,1,5]
+
+const nextPermutation = (nums)=>{
+    function reverse(start) {
+        let end = nums.length - 1
+        while (start < end) {
+            [nums[start], nums[end]] = [nums[end], nums[start]];
+            start++
+            end--
+        }
+    }
+    
+    
+    let i = nums.length - 2;
+    while (i >= 0 && nums[i + 1] <= nums[i]) {
+        i--;
+    }
+    if (i < 0) {
+        reverse(0);
+        return;
+    }
+    let j = nums.length - 1;
+    while (j >= 0 && nums[j] <= nums[i]) {
+        j--;
+    }
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+    reverse(i + 1);
+    return nums;
+}
+
+console.log("reverse",nextPermutation(nums))
