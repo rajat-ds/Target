fd85534da9378bab0defeeab3e134fa97276cd4f
rajat-ds <rajatkumarbind@gmail.com>
2021-05-10 01:14:31 +0530

    pascal triangle 2

--- day2-Arrays/pascalTriangle.js
+++ day2-Arrays/pascalTriangle.js
@@ -11,4 +11,21 @@ const printPascalTriangle = (n) => {
   return ans;
 };
 
-console.log(printPascalTriangle(5));
+// console.log(printPascalTriangle(5));
+
+// Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+const printPascalTriangle2 = (n) => {
+  if (!n) return [1];
+  let ans = [1];
+  for (let i = 0; i < n; i++) {
+    let temp = ans[i];
+    temp *= n - i;
+    temp /= i + 1;
+    ans.push(temp);
+  }
+
+  return ans;
+};
+
+console.log(printPascalTriangle2(3));
