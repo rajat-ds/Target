84f99f268644697d2475cd577d25e716dc4bc5c7
rajat-ds <rajatkumarbind@gmail.com>
2021-05-10 01:14:59 +0530

    pascal triangle 2 in allInOne

--- all_In_One.js
+++ all_In_One.js
@@ -153,3 +153,35 @@ var setZeroes = function (matrix) {
     if (Col) matrix[i][0] = 0;
   }
 };
+
+const printPascalTriangle = (n) => {
+  let ans = [];
+  for (let i = 0; i < n; i++) {
+    ans[i] = [];
+    ans[i][0] = 1;
+    for (let k = 1; k < i; k++) {
+      ans[i][k] = ans[i - 1][k - 1] + ans[i - 1][k];
+    }
+    ans[i][i] = 1;
+  }
+  return ans;
+};
+
+// console.log(printPascalTriangle(5));
+
+// Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+const printPascalTriangle2 = (n) => {
+  if (!n) return [1];
+  let ans = [1];
+  for (let i = 0; i < n; i++) {
+    let temp = ans[i];
+    temp *= n - i;
+    temp /= i + 1;
+    ans.push(temp);
+  }
+
+  return ans;
+};
+
+console.log(printPascalTriangle2(3));
