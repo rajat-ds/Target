56b9b890d6064e4bdaea111dda855748043b0f61
rajat-ds <rajatkumarbind@gmail.com>
2021-05-13 01:28:34 +0530

    search 2d matrix binary search approach

--- day3/search2DMatrix.js
+++ day3/search2DMatrix.js
@@ -20,4 +20,38 @@
 
 matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 33
 
-console.log(searchMatrix(matrix,target))
+// console.log(searchMatrix(matrix,target))
+
+const searchMatrix2=(matrix,target)=>{
+    let row = matrix.length;
+    let col = matrix[0].length;
+    let left=0 ;
+    let right = row*col - 1;
+    if (!matrix.length || !matrix[0].length) return false;
+     while(left<=right){
+         let mid = Math.floor((left+right)/2);
+         if(target==matrix[Math.floor(mid/col)][mid%col])
+                 return true;
+         if(target > matrix[Math.floor(mid/col)][mid%col])
+                 left = mid + 1;
+         else 
+                  right = mid -1;        
+                       
+
+
+
+     }
+     return false;
+    
+
+
+ 
+
+
+
+
+
+}
+// matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 33
+
+console.log(searchMatrix2(matrix,33))
