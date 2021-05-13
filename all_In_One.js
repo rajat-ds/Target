a8014113eac8a2dd385ac9c6906f3adc57f6a9ad
rajat-ds <rajatkumarbind@gmail.com>
2021-05-14 01:55:54 +0530

    Pow(x, n)

--- all_In_One.js
+++ all_In_One.js
@@ -256,3 +256,22 @@ const searchMatrix2 = (matrix, target) => {
   }
   return false;
 };
+
+const pow = (x, n) => {
+  if (n === 0) return 1;
+  let ans = 1;
+  let nn = n;
+  if (n < 0) nn *= -1;
+  while (nn > 0) {
+    if (nn % 2 === 0) {
+      x *= x;
+      nn /= 2;
+    } else {
+      ans *= x;
+      nn -= 1;
+    }
+  }
+  if (n < 0) ans = 1 / ans;
+
+  return ans;
+};
