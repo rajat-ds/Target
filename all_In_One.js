2ed3461b6582cd57380eec8989cee682da81d9c3
rajat-ds <rajatkumarbind@gmail.com>
2021-05-15 00:56:32 +0530

    majority element 2

--- all_In_One.js
+++ all_In_One.js
@@ -286,3 +286,35 @@ var majorityElement = function(nums) {
   })
   return ele;
 };
+
+const majorityElement = function (nums) {
+  let num1 = -1;
+  let num2 = -1;
+  let count1 = 0;
+  let count2 = 0;
+  let ans = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === num1) count1++;
+    else if (nums[i] === num2) count2++;
+    else if (!count1) {
+      count1 = 1;
+      num1 = nums[i];
+    } else if (!count2) {
+      count2 = 1;
+      num2 = nums[i];
+    } else {
+      count1--;
+      count2--;
+    }
+  }
+  count1 = 0;
+  count2 = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === num1) count1++;
+    else if (nums[i] === num2) count2++;
+  }
+  if (count1 > Math.floor(nums.length / 3)) ans.push(num1);
+  if (count2 > Math.floor(nums.length / 3)) ans.push(num2);
+
+  return ans;
+};
