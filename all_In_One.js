4a940df3df01f356cdbf936e1f4566eb57e36074
rajat-ds <rajatkumarbind@gmail.com>
2021-08-05 10:08:19 +0530

    powerSet (print all subsequences)

--- all_In_One.js
+++ all_In_One.js
@@ -318,3 +318,17 @@ const majorityElement = function (nums) {
 
   return ans;
 };
+
+const powerSet = (str) => {
+  let len = str.length;
+  let ans = [];
+  for (let i = 0; i < 1 << len; i++) {
+    let substr = [];
+    for (let j = 0; j < len; j++) {
+      if ((i & (1 << j)) != 0) substr.push(str[j]);
+    }
+    ans.push(substr);
+  }
+
+  return ans;
+};
