8b06f029ee8774faa21d887019c4bc9560faf8d5
rajat-ds <rajatkumarbind@gmail.com>
2021-08-06 12:59:35 +0530

    sliding window maximum

--- all_In_One.js
+++ all_In_One.js
@@ -332,3 +332,17 @@ const powerSet = (str) => {
 
   return ans;
 };
+
+const maxSlidingWindow =(list,k)=>{
+  let ans=[]
+  let deque=[]
+  for (let i=0;i<list.length;i++ ){
+    if(deque.length && deque[0]==i-k)
+        deque.shift()
+    while(deque.length && list[deque[deque.length-1]] < list[i])
+        deque.pop()
+    deque.push(i)    
+    if(i>=k-1) ans.push(list[deque[0]])        
+  }
+  return ans
+}
