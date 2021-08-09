c92ddd22d9d0c8e4a03e33010a9e8d1774b027d6
rajat-ds <rajatkumarbind@gmail.com>
2021-08-10 01:42:27 +0530

    unique path

--- all_In_One.js
+++ all_In_One.js
@@ -419,3 +419,25 @@ const fourSumProblem = (list, target) => {
   }
   return result;
 };
+
+var uniquePaths = function (m, n) {
+  const memo = new Array(m).fill(0);
+  for (let i = 0; i < memo.length; i++) {
+    memo[i] = new Array(n).fill(-1);
+  }
+  return helper(m, n, 0, 0, memo);
+};
+
+const helper = (m, n, row, col, memo) => {
+  if (row === m - 1 && col === n - 1) return 1;
+  if (row >= m || col >= n) return 0;
+
+  if (memo[row][col] === -1) {
+    const pathsRight = helper(m, n, row, col + 1, memo);
+    const pathsDown = helper(m, n, row + 1, col, memo);
+
+    memo[row][col] = pathsRight + pathsDown;
+  }
+
+  return memo[row][col];
+};
---
+++ day3/uniquePaths.js
@@ -0,0 +1,31 @@
+//https://www.youtube.com/watch?v=t_f0nwwdg5o&list=PLgUwDviBIf0rPG3Ictpu74YWBQ1CaBkm2&index=18
+const countPath = (i, j, m, n) => {
+  if (i == m - 1 && j == n - 1) return 1;
+  else if (i >= m || j >= n) return 0;
+  else return countPath(i + 1, j, m, n) + countPath(i, j + 1, m, n);
+};
+
+console.log(countPath(0, 0, 3, 7));
+
+var uniquePaths = function (m, n) {
+  const memo = new Array(m).fill(0);
+  for (let i = 0; i < memo.length; i++) {
+    memo[i] = new Array(n).fill(-1);
+  }
+  return helper(m, n, 0, 0, memo);
+};
+
+const helper = (m, n, row, col, memo) => {
+  if (row === m - 1 && col === n - 1) return 1;
+  if (row >= m || col >= n) return 0;
+
+  if (memo[row][col] === -1) {
+    const pathsRight = helper(m, n, row, col + 1, memo);
+    const pathsDown = helper(m, n, row + 1, col, memo);
+
+    memo[row][col] = pathsRight + pathsDown;
+  }
+
+  return memo[row][col];
+};
+console.log(uniquePaths(3, 7));
