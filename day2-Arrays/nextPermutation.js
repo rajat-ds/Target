033b17a0fdc597fb593d55485faaddd9b9a6f142
Mr. Kashyap <rajatkumarbind@gmail.com>
2023-12-15 02:25:20 +0530

    next permutation code formatted

--- day2-Arrays/nextPermutation.js
+++ day2-Arrays/nextPermutation.js
@@ -1,31 +1,30 @@
-const nums= [1,1,5]
+const nums = [1, 1, 5];
 
-const nextPermutation = (nums)=>{
-    function reverse(start) {
-        let end = nums.length - 1
-        while (start < end) {
-            [nums[start], nums[end]] = [nums[end], nums[start]];
-            start++
-            end--
-        }
+const nextPermutation = (nums) => {
+  function reverse(start) {
+    let end = nums.length - 1;
+    while (start < end) {
+      [nums[start], nums[end]] = [nums[end], nums[start]];
+      start++;
+      end--;
     }
-    
-    
-    let i = nums.length - 2;
-    while (i >= 0 && nums[i + 1] <= nums[i]) {
-        i--;
-    }
-    if (i < 0) {
-        reverse(0);
-        return;
-    }
-    let j = nums.length - 1;
-    while (j >= 0 && nums[j] <= nums[i]) {
-        j--;
-    }
-      [nums[i], nums[j]] = [nums[j], nums[i]];
-    reverse(i + 1);
-    return nums;
-}
+  }
 
-console.log("reverse",nextPermutation(nums))
+  let i = nums.length - 2;
+  while (i >= 0 && nums[i + 1] <= nums[i]) {
+    i--;
+  }
+  if (i < 0) {
+    reverse(0);
+    return;
+  }
+  let j = nums.length - 1;
+  while (j >= 0 && nums[j] <= nums[i]) {
+    j--;
+  }
+  [nums[i], nums[j]] = [nums[j], nums[i]];
+  reverse(i + 1);
+  return nums;
+};
+
+console.log("reverse", nextPermutation(nums));
