a2b71627ab527af5878959e00849ad78f6824294
Mr. Kashyap <rajatkumarbind@gmail.com>
2024-01-12 22:22:57 +0530

    merge two sorted arrays

--- arrays/merge_two_sorted_arrays.js
+++ arrays/merge_two_sorted_arrays.js
@@ -52,4 +52,21 @@ const mergeSortedArrayInsertion = (arr1, arr2) => {
   return { arr1, arr2 };
 };
 
+var merge = function (nums1, m, nums2, n) {
+  let first = m - 1,
+    second = n - 1;
+  for (i = m + n - 1; i >= 0; i--) {
+    if (second < 0) {
+      break;
+    }
+    if (nums1[first] > nums2[second]) {
+      nums1[i] = nums1[first];
+      first--;
+    } else {
+      nums1[i] = nums2[second];
+      second--;
+    }
+  }
+  return nums1;
+};
 //console.log(mergeSortedArrayInsertion(arr1, arr2))
