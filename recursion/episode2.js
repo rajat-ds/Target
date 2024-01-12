0c9d7940ecfee4e3383e1e40a6bd542309fc1f46
Mr. Kashyap <rajatkumarbind@gmail.com>
2024-01-12 22:49:59 +0530

    code formatted

--- recursion/episode2.js
+++ recursion/episode2.js
@@ -8,35 +8,34 @@ const printNameNTimes = (i, n, name) => {
 
 //printNameNTimes(1, 5, "rajat");
 
-// print Linearly from 1 to n 
-const printOneToN = ( i , n ) =>{
-    if(i > n ) return
-    console.log(i)
-    printOneToN(i+1,n)
-}
+// print Linearly from 1 to n
+const printOneToN = (i, n) => {
+  if (i > n) return;
+  console.log(i);
+  printOneToN(i + 1, n);
+};
 // printOneToN(1,5)
 
 //print linealy one to N using backtracking
-const printOneToNBackTracking =  ( i , n ) =>{
-    if( !i ) return 
-    printOneToNBackTracking( i - 1 , n)
-    console.log(i)
-}
+const printOneToNBackTracking = (i, n) => {
+  if (!i) return;
+  printOneToNBackTracking(i - 1, n);
+  console.log(i);
+};
 // printOneToNBackTracking(5,5)
 
-//print linearly from N to 1 
-const printNTone = ( i , n ) =>{
-    if(!i) return 0 ;
-    console.log(i)
-    printNTone(i-1,n)
-}
+//print linearly from N to 1
+const printNTone = (i, n) => {
+  if (!i) return 0;
+  console.log(i);
+  printNTone(i - 1, n);
+};
 // printNTone(5,5)
 
 //print linarly from N to 1 using back tracking
-
-const printNToOneBackTracking = ( i , n ) =>{
-    if( i > n ) return 
-    printNToOneBackTracking( i + 1 ,n)
-    console.log( i )
-}
-printNToOneBackTracking(1,6)
+const printNToOneBackTracking = (i, n) => {
+  if (i > n) return;
+  printNToOneBackTracking(i + 1, n);
+  console.log(i);
+};
+// printNToOneBackTracking(1,6)
