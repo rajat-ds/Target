bdbef063d1f3aaa502a218c47ead0bae4a27c596
Mr. Kashyap <rajatkumarbind@gmail.com>
2024-01-12 22:49:17 +0530

    Episode 1 and 2

--- recursion/episode1.js
+++ recursion/episode1.js
@@ -1,1 +1,3 @@
 // Recursion : When a function call itself with a certain specified condition is known as recursion .
+// When it is not called with speicified condtion then function context keeps on storing in stack and that causes stack overflow .
+//  This is also know as segmentation fault.
