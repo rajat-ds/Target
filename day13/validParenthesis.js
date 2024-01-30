279f727399b0efd392dccd6ed4fcbd558ee293dd
Mr. Kashyap <rajatkumarbind@gmail.com>
2024-01-31 00:22:24 +0530

    valid parenthesis

---
+++ day13/validParenthesis.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  if (!s.length) return false;
+  let temp = [...s];
+  let stacks = [];
+  for (let i = 0; i < temp.length; i++) {
+    if (["[", "{", "("].includes(temp[i])) {
+      stacks.push(temp[i]);
+      continue;
+    }
+    let check;
+    switch (temp[i]) {
+      case "}":
+        check = stacks.pop();
+        if (check !== "{") return false;
+        break;
+
+      case "]":
+        check = stacks.pop();
+        if (check !== "[") return false;
+        break;
+
+      case ")":
+        check = stacks.pop();
+        if (check !== "(") return false;
+        break;
+    }
+  }
+  return stacks.length == 0;
+};
