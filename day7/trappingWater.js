57c3e7560d5b6902fabda39faa3dcf25be1e53c6
Mr. Kashyap <rajatkumarbind@gmail.com>
2024-01-30 23:52:23 +0530

    trapping water

--- day7/trappingWater.js
+++ day7/trappingWater.js
@@ -23,3 +23,23 @@ var trap = function (height) {
   }
   return res;
 };
+
+var trap1 = function (height) {
+  let prefix = [];
+  let postfix = [];
+  let max = 0;
+  for (let i = 0; i < height.length; i++) {
+    max = Math.max(height[i], max);
+    prefix[i] = max;
+  }
+  max = 0;
+  for (let i = height.length - 1; i >= 0; i--) {
+    max = Math.max(height[i], max);
+    postfix[i] = max;
+  }
+  let sum = 0;
+  for (let i = 0; i < height.length; i++) {
+    sum += Math.abs(height[i] - Math.min(prefix[i], postfix[i]));
+  }
+  return sum;
+};
