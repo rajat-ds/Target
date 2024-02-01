b3d8c4652c9de196cb0f7eb761431522bd489c88
Mr. Kashyap <rajatkumarbind@gmail.com>
2024-02-02 01:08:52 +0530

    max heap

--- day12(Bits manipulation)/maxHeap.js
+++ day12(Bits manipulation)/maxHeap.js
@@ -1,8 +1,8 @@
 const maxHeapify = (arr, i, heapLength) => {
-  let LC = 2 * i;
-  let RC = 2 * i + 1;
+  let LC = 2 * i + 1;
+  let RC = 2 * i + 2;
   let largest;
-
+  
   if (LC <= heapLength && arr[LC] > arr[i]) largest = LC;
   else largest = i;
 
@@ -17,13 +17,13 @@ const maxHeapify = (arr, i, heapLength) => {
   }
 };
 
-arr = [0, 1, 5, 6, 8, 12, 14, 16];
+arr = [1, 5, 6, 8, 12, 14, 16];
 
 // arr = [ 0 ,9 ,6 ,5 ,0 ,8 ,2 ,7 ,1 ,3 ]
 
 const buildHeap = (arr) => {
   let heapLength = arr.length;
-  for (let i = arr.length >> 1; i >= 1; i--) {
+  for (let i = arr.length >> 1; i >= 0; i--) {
     maxHeapify(arr, i, heapLength);
   }
 };
