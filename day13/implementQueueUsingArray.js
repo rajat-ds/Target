b9d283e6d6a0866c5437671b0fa7e3b310bf2f3f
Mr. Kashyap <rajatkumarbind@gmail.com>
2024-02-01 11:13:12 +0530

    implementQueueUsingArray

---
+++ day13/implementQueueUsingArray.js
@@ -0,0 +1,29 @@
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(item) {
+    this.items.push(item);
+  }
+
+  dequeue() {
+    return this.items.shift();
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+}
+
+let queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+console.log(queue.dequeue()); // 1
+console.log(queue.dequeue()); // 2
+console.log(queue.dequeue()); // 3
