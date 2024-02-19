3bb73634d253e0aaa747806afdbda311cc2d5cb1
Mr. Kashyap <rajatkumarbind@gmail.com>
2024-02-19 18:11:28 +0530

    n meeting in one room

--- day8/N_MeetingInOneRoom.js
+++ day8/N_MeetingInOneRoom.js
@@ -1,26 +1,17 @@
-function maxMeetings(s, f, n) {
-  let a = new Array(n + 1);
-  let i;
-  for (i = 0; i < n; i++) {
-    a[i] = new Array(2);
-    a[i][0] = f[i];
-    a[i][1] = i;
-  }
-  a.sort();
-  let time_limit = a[0][0];
-  let m = [];
-
-  m.push(a[0][1] + 1);
-  for (i = 1; i < n; i++) {
-    if (s[a[i][1]] > time_limit) {
-      m.push(a[i][1] + 1);
-      time_limit = a[i][0];
+const maxMeetings = (start, end, n) => {
+  let arr = [];
+  for (let i = 0; i < n; i++) arr.push([start[i], end[i]]);
+  arr.sort((a, b) => a[1] - b[1]);
+  let last = arr[0][1];
+  let count = 1;
+  for (let i = 1; i < n; i++) {
+    if (arr[i][0] > last) {
+      last = arr[i][1];
+      count++;
     }
   }
-  for (let i = 0; i < m.length; i++) {
-    document.write(m[i], " ");
-  }
-}
+  return count;
+};
 let s = [1, 3, 0, 5, 8, 5];
 let f = [2, 4, 6, 7, 9, 9];
 let n = s.length;
