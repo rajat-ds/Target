50b651466f476caf703ae2b1cc7d062840ac100d
Mr. Kashyap <rajatkumarbind@gmail.com>
2024-03-17 03:17:14 +0530

    merge two sorted array

--- arrays/merge_two_sorted_arrays.js
+++ arrays/merge_two_sorted_arrays.js
@@ -69,4 +69,42 @@ var merge = function (nums1, m, nums2, n) {
   }
   return nums1;
 };
+
+var merge = function (nums1, m, nums2, n) {
+  for (let i = m; i < m + n; i++) {
+    nums1[i] = nums2[i - m];
+  }
+  let gap = ((m + n) >> 1) + ((m + n) % 2);
+  while (true) {
+    let i = 0;
+    while (i + gap < m + n) {
+      if (nums1[i] > nums1[i + gap])
+        [nums1[i], nums1[i + gap]] = [nums1[i + gap], nums1[i]];
+      i++;
+    }
+    if (gap === 1) break;
+    else {
+      gap = (gap >> 1) + (gap % 2);
+    }
+  }
+  return nums1;
+};
+
+var merge = function (nums1, m, nums2, n) {
+  let first = m - 1,
+    second = n - 1;
+  for (i = m + n - 1; i >= 0; i--) {
+    if (second < 0) {
+      break;
+    }
+    if (nums1[first] > nums2[second]) {
+      nums1[i] = nums1[first];
+      first--;
+    } else {
+      nums1[i] = nums2[second];
+      second--;
+    }
+  }
+  return nums1;
+};
 //console.log(mergeSortedArrayInsertion(arr1, arr2))
